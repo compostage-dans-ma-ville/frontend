32903dbe1cadbd14b30737a262b5faecfa083678
johannchopin <31794680+johannchopin@users.noreply.github.com>
2023-05-24 21:38:04 +0200

    feat: what to compost

---
+++ src/components/site/WhatToCompost/WhatToCompost.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded'
+import ThumbDownAltRoundedIcon from '@mui/icons-material/ThumbDownAltRounded'
+import ThumbsUpDownRoundedIcon from '@mui/icons-material/ThumbsUpDownRounded'
+import ThumbUpAltRoundedIcon from '@mui/icons-material/ThumbUpAltRounded'
+import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import Box, { BoxProps } from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
+import Link from 'next/link'
+import { useTranslation } from 'next-i18next'
+
+import WhatToCompostList from './WhatToCompostList'
+
+export interface WhatToCompostProps extends BoxProps {}
+
+const WhatToCompost: React.FC<WhatToCompostProps> = ({ ...props }) => {
+  const { t } = useTranslation([
+    'common',
+    'pages',
+    'whatToCompost'
+  ])
+
+  const withoutHesitation: Record<string, string[] | null> = t('whatToCompost:without_hesitation', { returnObjects: true })
+  const withModeration: Record<string, string[] | null> = t('whatToCompost:with_moderation', { returnObjects: true })
+  const toAvoid: Record<string, string[] | null> = t('whatToCompost:to_avoid', { returnObjects: true })
+
+  return (
+    <Box mt={2} mx={2} {...props}>
+      <Typography variant='h5' component="h2" color="primary.main" display="flex" alignItems="center">
+        {t('pages:site.what_to_compost')}
+      </Typography>
+
+      <Box mt={2}>
+        <Accordion sx={{ borderColor: 'success.main' }}>
+          <AccordionSummary expandIcon={<ExpandMoreRoundedIcon />}>
+            <Typography variant='h6' component="h3" display="flex" alignItems="center">
+              <ThumbUpAltRoundedIcon sx={{ color: 'success.main' }} />
+              <Typography component="span" ml={1} fontWeight="bold">
+                {t('pages:site.without_hesitation')}
+              </Typography>
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <WhatToCompostList items={withoutHesitation}/>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion sx={{ borderColor: 'warning.main' }}>
+          <AccordionSummary expandIcon={<ExpandMoreRoundedIcon />}>
+            <Typography variant='h6' component="h3" display="flex" alignItems="center">
+              <ThumbsUpDownRoundedIcon sx={{ color: 'warning.main' }} />
+              <Typography component="span" ml={1} fontWeight="bold">
+                {t('pages:site.with_moderation')}
+              </Typography>
+            </Typography>
+
+          </AccordionSummary>
+          <AccordionDetails>
+            <WhatToCompostList items={withModeration}/>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion sx={{ borderColor: 'error.main' }}>
+          <AccordionSummary expandIcon={<ExpandMoreRoundedIcon />}>
+            <Typography variant='h6' component="h3" display="flex" alignItems="center">
+              <ThumbDownAltRoundedIcon sx={{ color: 'error.main' }} />
+              <Typography component="span" ml={1} fontWeight="bold">
+                {t('pages:site.to_avoid')}
+              </Typography>
+            </Typography>
+
+          </AccordionSummary>
+          <AccordionDetails>
+            <WhatToCompostList items={toAvoid}/>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+
+      <Grid container justifyContent="flex-end">
+        <Link href="https://librairie.ademe.fr/cadic/6991/guide-reussir-compost.pdf" target='_blank'>
+          <Button color='secondary'>
+            <Typography color="secondary.main" fontStyle="italic">{t('common:source')}: Ademe</Typography>
+          </Button>
+        </Link>
+      </Grid>
+    </Box>
+  )
+}
+
+export default WhatToCompost
---
+++ src/components/site/WhatToCompost/WhatToCompostList.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import Typography from '@mui/material/Typography'
+
+export interface WhatToCompostListProps {
+  items: Record<string, string[] | null>
+}
+const WhatToCompostList: React.FC<WhatToCompostListProps> = ({ items }) => {
+  return (
+    <List sx={{ my: 0 }}>
+      {Object.entries(items).map(([title, entries]) => (
+        <React.Fragment key={title}>
+          <ListItem sx={{ px: 0 }}>
+            <Typography fontWeight="bold" component="h4" variant='body1'>{title}</Typography>
+          </ListItem>
+          {entries && (
+            <List sx={{ p: 0, pl: 2, listStyleType: 'disc' }}>
+              {entries.map((entry) => (
+                <ListItem key={title + entry} sx={{ py: 0 }}>
+                  <Typography margin={0} sx={{ display: 'list-item' }}>{entry}</Typography>
+                </ListItem>
+              ))}
+            </List>
+          )}
+        </React.Fragment>
+      ))}
+
+    </List>
+  )
+}
+
+export default WhatToCompostList
